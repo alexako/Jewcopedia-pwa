07e0cedc2d0a39893d9664b5aa39a5668fdebfcb
kludgebot <akosialexpo@gmail.com>
2023-08-18 00:33:53 -0700

    ui: Style focused entry

--- src/FocusedEntry/FocusedEntry.jsx
+++ src/FocusedEntry/FocusedEntry.jsx
@@ -1,3 +1,4 @@
+import "./FocusedEntry.css";
 
 const FocusedEntry = ({ focusedEntry }) => {
 
@@ -5,8 +6,20 @@ const FocusedEntry = ({ focusedEntry }) => {
     <div className="focused-entry">
       {focusedEntry ? (
         <>
-          <div className="focused-entry__name">{focusedEntry.firstName} {focusedEntry.lastName}</div>
-          <div className="focused-entry__details">{focusedEntry.details}</div>
+          <div className="focused-entry__header">
+            { focusedEntry.avatar && (
+              <div className="focused-entry__avatar">
+                  <img className="focused-entry__avatar--img" src={focusedEntry.avatar} alt=""/>
+              </div>
+            )}
+            <div className="focused-entry__name">
+              {focusedEntry.firstName} {focusedEntry.lastName}
+            </div>
+          </div>
+          <div className="focused-entry__details">
+            <div className="focused-entry__details-title">About</div>
+            {focusedEntry.details}
+          </div>
         </>
       ) : (
         <div className="focused-entry__unselected">No entry selected</div>
