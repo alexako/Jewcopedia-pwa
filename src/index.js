39e43d98340046faebed0e60aa85d1845e4c2211
kludgebot <akosialexpo@gmail.com>
2023-10-24 00:17:33 -0700

    add: App version

--- src/index.js
+++ src/index.js
@@ -27,6 +27,12 @@ root.render(
   </React.StrictMode>
 );
 
+console.group();
+console.log("APP NAME:", "Jewcopedia");
+console.log("APP VERSION:", process.env.REACT_APP_VERSION);
+console.groupEnd();
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
